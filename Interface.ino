9cf4c3b75fb50a6807426cdbc303217b3b605d50
Jing Huan <124416920+jhjh0409@users.noreply.github.com>
2024-04-12 02:28:23 +0800

    Added MPU6050 sensors
    Implemented gyroscope and accelerometer sensors

--- Interface.ino
+++ Interface.ino
@@ -95,12 +95,42 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   tft.println("Hello World.");
   digitalWrite(firstScreenCS, HIGH);
+
+  setMPU6050();
 }
 
 void loop() {
   digitalWrite(secondScreenCS, LOW);
   static uint32_t scanTime = millis();
   uint16_t t_x = 9999, t_y = 9999; // To store the touch coordinates
+  unsigned long t1 = micros();
+
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
 
   // Scan keys every 50ms at most
   if (millis() - scanTime >= 50) {
@@ -114,6 +144,7 @@ void loop() {
       goBack = true;
     }
 
+    // If mode is on lesson, check lesson
     if (mode == "lesson" and pressed == false)
     {
       for (uint8_t b = 0; b < lsnCount; b++) {
@@ -264,7 +295,8 @@ void timerCount() {
       if (remainingMinutes > 0) {
         remainingMinutes--;
         remainingSeconds = 59;
-      } else {
+      } 
+      else {
         // Display "Time's up!" when countdown is finished
         tft.fillScreen(TFT_RED); // Clear the screen
         tft.setTextColor(TFT_WHITE); // Set text color to red
@@ -279,7 +311,8 @@ void timerCount() {
         previousMillis = 0;
         remainingMinutes = countdownMinutes;
       }
-    } else {
+    } 
+    else {
       remainingSeconds--;
     }
     if (mode != "menu") {
@@ -499,7 +532,7 @@ void btn7_pressAction(void)
   if (btn7.justPressed()) {
     Serial.println("btn7 button just pressed");
     digitalWrite(firstScreenCS, LOW);
-    tft.fillScreen(TFT_GREENYELLOW);
+    tft.fillScreen(TFT_CYAN);
     digitalWrite(firstScreenCS, HIGH);
     lessonScreen();
   }
@@ -528,7 +561,7 @@ void btn8_pressAction(void)
   if (btn8.justPressed()) {
     Serial.println("btn8 button just pressed");
     digitalWrite(firstScreenCS, LOW);
-    tft.fillScreen(TFT_ORANGE);
+    tft.fillScreen(TFT_VIOLET);
     digitalWrite(firstScreenCS, HIGH);
     lessonScreen();
   }
@@ -641,11 +674,89 @@ void initButtons() {
   btn6.setPressAction(btn6_pressAction);
   btn6.setReleaseAction(btn6_releaseAction);
 
-  btn7.initButtonUL(80, 120, BUTTON_W2, BUTTON_H2, TFT_WHITE, TFT_BLACK, TFT_GREENYELLOW, "Repeat", 2);
+  btn7.initButtonUL(80, 120, BUTTON_W2, BUTTON_H2, TFT_WHITE, TFT_BLACK, TFT_CYAN, "Repeat", 2);
   btn7.setPressAction(btn7_pressAction);
   btn7.setReleaseAction(btn7_releaseAction);
 
-  btn8.initButtonUL(200, 120, BUTTON_W2, BUTTON_H2, TFT_WHITE, TFT_BLACK, TFT_ORANGE, "Question", 2);
+  btn8.initButtonUL(200, 120, BUTTON_W2, BUTTON_H2, TFT_WHITE, TFT_BLACK, TFT_VIOLET, "Question", 2);
   btn8.setPressAction(btn8_pressAction);
   btn8.setReleaseAction(btn8_releaseAction);
+}
+
+void setMPU6050() {
+  Wire.setPins(sda_pin, scl_pin); // Set the I2C pins before begin
+  Wire.begin(); // join i2c bus (address optional for master)
+
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
 }
