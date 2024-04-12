efe010212507a2177b93bcfef6a11abd52daf330
Jing Huan <124416920+jhjh0409@users.noreply.github.com>
2024-04-13 00:37:16 +0800

    Added change display test
    Testing function to change display upon rotation

---
+++ DoubleDisplay_Touch.ino
@@ -0,0 +1,544 @@
+/*
+  The TFT_eSPI library incorporates an Adafruit_GFX compatible
+  button handling class, this sketch is based on the Arduin-o-phone
+  example.
+
+  This example displays a keypad where numbers can be entered and
+  sent to the Serial Monitor window.
+
+  The sketch has been tested on the ESP8266 (which supports SPIFFS)
+
+  The minimum screen size is 320 x 240 as that is the keypad size.
+*/
+
+// The SPIFFS (FLASH filing system) is used to hold touch screen
+// calibration data
+
+#include "FS.h"
+
+#include <SPI.h>
+#include <TFT_eSPI.h>      // Hardware-specific library
+
+#define firstScreenCS 9
+#define secondScreenCS 10
+
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+int sda_pin = 17; // GPIO8 as I2C SDA
+int scl_pin = 18; // GPIO9 as I2C SCL
+
+bool disp = false;
+bool hasRun = false;
+int timer = 0;
+
+// This is the file name used to store the calibration data
+// You can change this to create new calibration files.
+// The SPIFFS file name must start with "/".
+#define CALIBRATION_FILE "/TouchCalData1"
+
+// Set REPEAT_CAL to true instead of false to run calibration
+// again, otherwise it will only be done once.
+// Repeat calibration if you change the screen rotation.
+#define REPEAT_CAL false
+
+// Keypad start position, key sizes and spacing
+#define KEY_X 40 // Centre of key
+#define KEY_Y 96
+#define KEY_W 62 // Width and height
+#define KEY_H 30
+#define KEY_SPACING_X 18 // X and Y gap
+#define KEY_SPACING_Y 20
+#define KEY_TEXTSIZE 1   // Font size multiplier
+
+// Using two fonts since numbers are nice when bold
+#define LABEL1_FONT &FreeSansOblique12pt7b // Key label font 1
+#define LABEL2_FONT &FreeSansBold12pt7b    // Key label font 2
+
+// Numeric display box size and location
+#define DISP_X 1
+#define DISP_Y 10
+#define DISP_W 238
+#define DISP_H 50
+#define DISP_TSIZE 3
+#define DISP_TCOLOR TFT_CYAN
+
+// Number length, buffer for storing it and character index
+#define NUM_LEN 12
+char numberBuffer[NUM_LEN + 1] = "";
+uint8_t numberIndex = 0;
+
+// We have a status line for messages
+#define STATUS_X 120 // Centred on this
+#define STATUS_Y 65
+
+int tolerance = 4;
+char* hor[4] = {"A", "B", "F", "E"};
+char* ver[4] = {"A", "C", "F", "D"};
+uint16_t hcolors[4] = {TFT_RED, TFT_GREEN, TFT_VIOLET, TFT_BLUE,};
+uint16_t vcolors[4] = {TFT_RED, TFT_YELLOW, TFT_VIOLET, TFT_ORANGE,};
+int horstate = 0;
+int verstate = 0;
+
+// Create 15 keys for the keypad
+char keyLabel[15][5] = {"New", "Del", "Send", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0", "#" };
+uint16_t keyColor[15] = {TFT_RED, TFT_DARKGREY, TFT_DARKGREEN,
+                         TFT_BLUE, TFT_BLUE, TFT_BLUE,
+                         TFT_BLUE, TFT_BLUE, TFT_BLUE,
+                         TFT_BLUE, TFT_BLUE, TFT_BLUE,
+                         TFT_BLUE, TFT_BLUE, TFT_BLUE
+                        };
+
+// Invoke the TFT_eSPI button class and create all the button objects
+TFT_eSPI_Button key[15];
+
+//------------------------------------------------------------------------------------------
+
+void setup() {
+  // Use serial port
+  Serial.begin(9600);
+
+  pinMode(firstScreenCS, OUTPUT);
+  digitalWrite(firstScreenCS, HIGH);
+  
+  pinMode(secondScreenCS, OUTPUT);
+  digitalWrite(secondScreenCS, HIGH);
+  
+  // We need to 'init' both displays
+  // at the same time. so set both cs pins low
+  digitalWrite(firstScreenCS, LOW);
+  digitalWrite(secondScreenCS, LOW);
+
+  // Initialise the TFT screen
+  tft.init();
+
+  // Set both cs pins HIGH, or 'inactive'
+  digitalWrite(firstScreenCS, HIGH);
+  digitalWrite(secondScreenCS, HIGH);
+
+  digitalWrite(secondScreenCS, LOW);
+
+  // Set the rotation before we calibrate
+  tft.setRotation(1);
+
+  // Calibrate the touch screen and retrieve the scaling factors
+  touch_calibrate();
+
+  // Clear the screen
+  tft.fillScreen(TFT_BLACK);
+
+  // Draw keypad background
+  tft.fillRect(0, 0, 240, 320, TFT_DARKGREY);
+
+  // Draw number display area and frame
+  tft.fillRect(DISP_X, DISP_Y, DISP_W, DISP_H, TFT_BLACK);
+  tft.drawRect(DISP_X, DISP_Y, DISP_W, DISP_H, TFT_WHITE);
+
+  // Draw keypad
+  drawKeypad();
+
+  digitalWrite(secondScreenCS, HIGH);
+
+  digitalWrite(firstScreenCS, LOW);
+  tft.setRotation(2);  
+  tft.fillScreen(TFT_GREEN);
+  tft.print("Screen calibrated");
+  // set the cs pin back to HIGH when finished
+  // with this particular display
+  digitalWrite(firstScreenCS, HIGH);
+
+  Wire.setPins(sda_pin, scl_pin); // Set the I2C pins before begin
+  Wire.begin(); // join i2c bus (address optional for master)
+
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  Serial.println("");
+  delay(100);
+  //tft.sleep(disp);
+}
+
+//------------------------------------------------------------------------------------------
+
+void loop(void) {
+
+  //timer++;
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  digitalWrite(secondScreenCS, LOW);
+  uint16_t t_x = 0, t_y = 0; // To store the touch coordinates
+
+  // Pressed will be set true is there is a valid touch on the screen
+  bool pressed = tft.getTouch(&t_x, &t_y);
+
+  // / Check if any key coordinate boxes contain the touch coordinates
+  for (uint8_t b = 0; b < 15; b++) {
+    if (pressed && key[b].contains(t_x, t_y)) {
+      key[b].press(true);  // tell the button it is pressed
+    } else {
+      key[b].press(false);  // tell the button it is NOT pressed
+    }
+  }
+
+  // Check if any key has changed state
+  for (uint8_t b = 0; b < 15; b++) {
+
+    if (b < 3) tft.setFreeFont(LABEL1_FONT);
+    else tft.setFreeFont(LABEL2_FONT);
+
+    if (key[b].justReleased()) key[b].drawButton();     // draw normal
+
+    if (key[b].justPressed()) {
+      key[b].drawButton(true);  // draw invert
+
+      // if a numberpad button, append the relevant # to the numberBuffer
+      if (b >= 3) {
+        if (numberIndex < NUM_LEN) {
+          numberBuffer[numberIndex] = keyLabel[b][0];
+          numberIndex++;
+          numberBuffer[numberIndex] = 0; // zero terminate
+        }
+        status(""); // Clear the old status
+      }
+
+      // Del button, so delete last char
+      if (b == 1) {
+        numberBuffer[numberIndex] = 0;
+        if (numberIndex > 0) {
+          numberIndex--;
+          numberBuffer[numberIndex] = 0;//' ';
+        }
+        status(""); // Clear the old status
+      }
+
+      if (b == 2) {
+        status("Sent value to serial port");
+        Serial.println(numberBuffer);
+      }
+      // we dont really check that the text field makes sense
+      // just try to call
+      if (b == 0) {
+        status("Value cleared");
+        numberIndex = 0; // Reset index to 0
+        numberBuffer[numberIndex] = 0; // Place null in buffer
+      }
+
+      // Update the number display field
+      tft.setTextDatum(TL_DATUM);        // Use top left corner as text coord datum
+      tft.setFreeFont(&FreeSans18pt7b);  // Choose a nice font that fits box
+      tft.setTextColor(DISP_TCOLOR);     // Set the font colour
+
+      // Draw the string, the value returned is the width in pixels
+      int xwidth = tft.drawString(numberBuffer, DISP_X + 4, DISP_Y + 12);
+
+      // Now cover up the rest of the line up by drawing a black rectangle.  No flicker this way
+      // but it will not work with italic or oblique fonts due to character overlap.
+      tft.fillRect(DISP_X + 4 + xwidth, DISP_Y + 1, DISP_W - xwidth - 5, DISP_H - 2, TFT_BLACK);
+
+      delay(10); // UI debouncing
+      digitalWrite(secondScreenCS, HIGH);
+
+      digitalWrite(firstScreenCS, LOW);
+      tft.setRotation(2);  
+      tft.fillScreen(TFT_GREEN);
+      tft.print(String(numberBuffer));
+      // set the cs pin back to HIGH when finished
+      // with this particular display
+      digitalWrite(firstScreenCS, HIGH);
+    }
+  }
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+
+
+  /*if (hasRun == false) {
+    checkDisp(a.acceleration.y);
+  }
+
+  // Display goes to sleep after count of 10000
+  if (timer > 1000 and a.acceleration.y => -0.5) {
+    timer = 0;
+    disp = false;
+    hasRun = false;
+    //tft.sleep(disp);
+  }*/
+
+  if (g.gyro.z >= tolerance) {
+    if (horstate - 1 < 0) {
+      horstate = 3;
+    }
+    else{
+      horstate = horstate - 1;
+    }
+    dispOptHor(horstate);
+  }
+  else if (g.gyro.z <= -tolerance) {
+    if (horstate + 1 > 3) {
+      horstate = 0;
+    }
+    else{
+      horstate = horstate + 1;
+    }
+    dispOptHor(horstate);
+  }
+  if (g.gyro.y >= tolerance) {
+    if (verstate - 1 < 0) {
+      verstate = 3;
+    }
+    else{
+      verstate = verstate - 1;
+    }
+    dispOptVer(verstate);
+  }
+  else if (g.gyro.y <= -tolerance) {
+    if (verstate + 1 > 3) {
+      verstate = 0;
+    }
+    else{
+      verstate = verstate + 1;
+    }
+    dispOptVer(verstate);
+  }
+
+  //Serial.print(timer);
+  Serial.print("\n");
+}
+
+//------------------------------------------------------------------------------------------
+
+void drawKeypad()
+{
+  // Draw the keys
+  for (uint8_t row = 0; row < 5; row++) {
+    for (uint8_t col = 0; col < 3; col++) {
+      uint8_t b = col + row * 3;
+
+      if (b < 3) tft.setFreeFont(LABEL1_FONT);
+      else tft.setFreeFont(LABEL2_FONT);
+
+      key[b].initButton(&tft, KEY_X + col * (KEY_W + KEY_SPACING_X),
+                        KEY_Y + row * (KEY_H + KEY_SPACING_Y), // x, y, w, h, outline, fill, text
+                        KEY_W, KEY_H, TFT_WHITE, keyColor[b], TFT_WHITE,
+                        keyLabel[b], KEY_TEXTSIZE);
+      key[b].drawButton();
+    }
+  }
+}
+
+//------------------------------------------------------------------------------------------
+
+void touch_calibrate()
+{
+  uint16_t calData[5];
+  uint8_t calDataOK = 0;
+
+  digitalWrite(firstScreenCS, LOW);
+  tft.setRotation(2);  
+  tft.fillScreen(TFT_RED);
+  tft.print("Screen calibrating");
+  // set the cs pin back to HIGH when finished
+  // with this particular display
+  digitalWrite(firstScreenCS, HIGH);
+
+  // check file system exists
+  if (!SPIFFS.begin()) {
+    Serial.println("formatting file system");
+    SPIFFS.format();
+    SPIFFS.begin();
+  }
+
+  // check if calibration file exists and size is correct
+  if (SPIFFS.exists(CALIBRATION_FILE)) {
+    if (REPEAT_CAL)
+    {
+      // Delete if we want to re-calibrate
+      SPIFFS.remove(CALIBRATION_FILE);
+    }
+    else
+    {
+      File f = SPIFFS.open(CALIBRATION_FILE, "r");
+      if (f) {
+        if (f.readBytes((char *)calData, 14) == 14)
+          calDataOK = 1;
+        f.close();
+      }
+    }
+  }
+
+  if (calDataOK && !REPEAT_CAL) {
+    // calibration data valid
+    tft.setTouch(calData);
+  } else {
+    // data not valid so recalibrate
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(20, 0);
+    tft.setTextFont(2);
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    tft.println("Touch corners as indicated");
+
+    tft.setTextFont(1);
+    tft.println();
+
+    if (REPEAT_CAL) {
+      tft.setTextColor(TFT_RED, TFT_BLACK);
+      tft.println("Set REPEAT_CAL to false to stop this running again!");
+    }
+
+    tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.println("Calibration complete!");
+
+    // store data
+    File f = SPIFFS.open(CALIBRATION_FILE, "w");
+    if (f) {
+      f.write((const unsigned char *)calData, 14);
+      f.close();
+    }
+  }
+}
+
+//------------------------------------------------------------------------------------------
+
+// Print something in the mini status bar
+void status(const char *msg) {
+  tft.setTextPadding(240);
+  //tft.setCursor(STATUS_X, STATUS_Y);
+  tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
+  tft.setTextFont(0);
+  tft.setTextDatum(TC_DATUM);
+  tft.setTextSize(1);
+  tft.drawString(msg, STATUS_X, STATUS_Y);
+}
+
+//------------------------------------------------------------------------------------------
+
+void checkDisp(float x) {
+  if (x < -0.5) {
+    disp = true;
+    Serial.print("screen waking");
+    hasRun = true;
+    setup();
+  }
+}
+
+void dispOptHor(int option) {
+  digitalWrite(firstScreenCS, LOW);
+  tft.fillScreen(hcolors[option]);
+  tft.setRotation(1);
+  tft.setCursor(150, 120);
+  tft.setTextSize(3);
+  tft.println(hor[option]);
+  digitalWrite(firstScreenCS, HIGH);
+}
+
+void dispOptVer(int option) {
+  digitalWrite(firstScreenCS, LOW);
+  tft.fillScreen(vcolors[option]);
+  tft.setRotation(1);
+  tft.setCursor(150, 120);
+  tft.setTextSize(3);
+  tft.println(ver[option]);
+  digitalWrite(firstScreenCS, HIGH);
+}
